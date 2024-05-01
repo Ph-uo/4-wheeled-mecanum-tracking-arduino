fb393dbeb18f1936d29672e9c8b0d332ddf9ff2a
Ph-uo <113018470+Ph-uo@users.noreply.github.com>
2024-05-01 16:13:10 +0700

    Update bam_quy_dao.ino 1/5

--- bam_quy_dao/bam_quy_dao.ino
+++ bam_quy_dao/bam_quy_dao.ino
@@ -12,7 +12,8 @@ double pos[3]={x,y,theta};
 void setup() {
   Serial.begin(9600);
   for(i=0;i<5;i++){pinMode(pwm[i],OUTPUT);}
-  
+  fw_kinematic[3][4]={[1,1,1,1];[-1,1,-1,1];[-1/(l/d),-1/(l-d),1/(l-d),1/(l-d)]}
+    
 }
 
 void loop() {
@@ -27,13 +28,13 @@ double quydao(t){//tinh toan quy dao chua xong
   yd= 0.9 + 0.7*sin(2*freq*t);
   thetad= 0; 
   w_spd[]={motor_speed(1),motor_speed(2),motor_speed(3),motor_speed(4)};
-  pos[]= {}
+  internal_mobile_spd= Matrix.Mutiply(w_spd,fw_kinematic,
 }
 double pid(){// chua co bo dieu khien pid
 
 }
 void motor_control(double speed, char ena,char in1,char in2){
-  speed=constrain(speed,0,255);
+  speed=constrain(speed,0,255);//hàm giới hạn
   analogwrite(ena,speed);
   if (speed>0){
     digitalwrite(in1,1);
@@ -47,14 +48,18 @@ void motor_control(double speed, char ena,char in1,char in2){
     digitalwrite(in2,0);
   }
 }
-double motor_speed(int counta){//tinh toan toc do cua 1 dong co
+double motor_speed(unsigned long count){//ham tinh toan toc do cua 1 dong co; input =xung encoder, output= toc do (m/s) va chieu quay
   double theta= count/(13*19.2);
   double speed_now= theta*t*60/1000;//m/s
+  if(){//xet chieu quay, chua co ham
+    
+  }
   return speed_now;
 }
 
+// thiet ke lai bo dem xung, chua khai bao bien
 void countpulse1a(){
-  count1a++;
+  unsigned long count1a++;
 }
 void countpulse1b(){
   count1b++;
